4817c54c818af75cf4d1853f45fe825d5302513b
RAHUL N P <81530030+NPR2002@users.noreply.github.com>
2024-02-02 23:06:22 +0530

    Add files via upload

---
+++ gemini_google.py
@@ -0,0 +1,14 @@
+!pip install -q -U google-generativeai
+
+import google.generativeai as genai
+
+genai.configure(api_key="Google api key")
+
+model = genai.GenerativeModel('gemini-pro')
+
+state = 1
+while(state):
+    response = model.generate_content(input("enter the question"))
+    print (response.text)
+    in1 =int(input("true = 1 or false = 0"))
+    state = in1
